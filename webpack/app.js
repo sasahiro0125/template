2f39eb36e3513fc7bc88963abc79ac441fe90f93
hiroaki.sasaki <sasa.hiro0125@gmail.com>
2015-09-01 01:33:28 +0900

    modify: test

--- webpack/app.js
+++ webpack/app.js
@@ -1,2 +1,7 @@
-require("jquery");
-require("velocity");
+require("angular");
+require("angular-animate");
+// require("famous-angular");
+require("ui-router");
+require("xlsx");
+// require('./src/scss/layout.scss');
+require('!css!sass!./src/scss/layout.scss');
